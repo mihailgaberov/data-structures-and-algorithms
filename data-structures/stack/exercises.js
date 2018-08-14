7148d535885a66d44756947362c1c81f47d4ec52
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-08-14 14:37:45 +0200

    Added isBalanced function for checking strings of brackets.

---
+++ data-structures/stack/exercises.js
@@ -0,0 +1,27 @@
+function isBalanced(str) {
+  let result = true
+  let stack = [];
+  
+	for (let prop in str) {
+    const char = str[prop]
+
+		if (['(', '['].includes(char)) {
+			stack.push(char)
+		} else {
+      if (stack.length === 0) result = false
+
+      const top = stack.pop()
+    
+      if ((top === '[' && char != ']') || (top === '(' && char != ')')) {
+        result = false
+      }
+    }
+  }
+  
+  return result
+}
+
+console.log(isBalanced('[]') === true)
+console.log(isBalanced('()[]()' === true))
+console.log(isBalanced('[](())(]') === false)
+console.log(isBalanced('((([])))[[[()]]]') === true)
