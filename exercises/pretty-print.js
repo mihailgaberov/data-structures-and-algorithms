f5f4c5ed86600ce7f356e82fe0e240dd7d45e80e
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-10-11 17:12:38 +0200

    Update pretty-print.js

--- exercises/pretty-print.js
+++ exercises/pretty-print.js
@@ -3,29 +3,36 @@ function prettyPrint(A) {
 
   if (A === 1) return [1];
 
-  const matrixRow = A * 2 - 1;
-  const res = [];
+  var matrixRow = A * 2 - 1;
+  console.log('matrixRow: ', matrixRow)
+  var res = [];
 
   // Upper Half 
-  for (let i = 0; i < Math.floor((matrixRow / 2)) + 1; i++) {
+  var upperHalfLimit =  Math.floor(matrixRow / 2) + 1
+  console.log('upperHalfLimit: ', upperHalfLimit);
+  for (var i = 0; i < upperHalfLimit; i++) {
+    console.log('upper part row:  ', i);
 
-    let m = A;
-    const row = [];
+    var m = A;
+    var row = [];
 
     // Decreasing part 
-    for (let j = 0; j < i; j++) {
+    for (var j = 0; j < i; j++) {
+      console.log('upper -> decreasing: ', m)
       row.push(m);
       m--;
     }
 
-    // Constant Part 
-    for (let k = 0; k < matrixRow - 2 * i; k++) {
+    // constant Part
+    for (var k = 0; k < matrixRow - 2 * i; k++) {
+      console.log('upper -> constant: ', A-i)
       row.push(A - i);
     }
 
     // Increasing part
     m = A - i + 1;
-    for (let l = 0; l < i; l++) {
+    for (var l = 0; l < i; l++) {
+      console.log('upper -> increasing: ', m)
       row.push(m);
       m++;
     }
@@ -34,25 +41,32 @@ function prettyPrint(A) {
   }
 
   // Lower Half 
-  for (let i = Math.floor(matrixRow / 2) - 1; i >= 0; i--) {
+  var lowerHalfLimit = Math.floor(matrixRow / 2) - 1;
+  console.log('lowerHalfLImit: ', lowerHalfLimit);
 
-    let m = A;
-    const row = [];
+  for (var i = lowerHalfLimit; i >= 0; i--) {
+    console.log('lower part row:  ', i);
+
+    var m = A;
+    var row = [];
 
     // Decreasing Part 
-    for (let j = 0; j < i; j++) {
+    for (var j = 0; j < i; j++) {
+      console.log('Lower -> decreasing: ', m)
       row.push(m);
       m--;
     }
 
     // Constant Part
-    for (let k = 0; k < matrixRow - 2 * i; k++) {
+    for (var k = 0; k < matrixRow - 2 * i; k++) {
+      console.log('Lower -> constant: ', A-i)
       row.push(A - i);
     }
 
     // Increasing Part 
     m = A - i + 1;
-    for (let l = 0; l < i; l++) {
+    for (var l = 0; l < i; l++) {
+      console.log('Lower -> Increasing: ', m)
       row.push(m);
       m++;
     }
@@ -63,7 +77,8 @@ function prettyPrint(A) {
   return res;
 }
 
-console.log(prettyPrint(2));
+// console.log(prettyPrint(1));
+// console.log(prettyPrint(2));
 console.log(prettyPrint(3));
-console.log(prettyPrint(4));
-console.log(prettyPrint(5));
+// console.log(prettyPrint(4));
+// console.log(prettyPrint(5));
