ff07dc7dd443ac3ef7e393687733dcd2d4bb500e
mihailgaberov <mihail.gaberov@gmail.com>
2018-10-12 15:31:29 +0200

    Adding numrange.js

---
+++ exercises/numrange.js
@@ -0,0 +1,9 @@
+function numRange(arr, start, end) {
+
+}
+
+// ex:
+// A : [10, 5, 1, 0, 2]
+// (B, C) : (6, 8)
+// --> 3
+// as [5, 1], [5, 1, 0], [5, 1, 0, 2] are the only 3 continuous subsequence with their sum in the range [6, 8]
